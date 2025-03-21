14068ce9b5b7ea38b91fab262007dc23c2f5bbb9
zaltechdev <hanifzalfa910@gmail.com>
2025-03-21 07:45:32 +0700

    update 21/03/25

--- js/animation.js
+++ js/animation.js
@@ -1,56 +1,4 @@
 $(document).ready(() => {
-    // Global delay animation when reloaded first time
-    setTimeout(() => {
-        // Logo animation script
-        $("#logo").removeClass("hidden")
-        $("#logo").addClass("flex")
-        setTimeout(() => {
-            $("#logo").removeClass("opacity-0")
-            $("#logo").removeClass("translate-y-7")
-        },100)
-        
-        // Menu navbar animation one-by-one
-        let delay = 100
-        $("#menu-bar > a").each(function (index, element) {
-            $(element).removeClass("hidden")
-            $(element).addClass("flex")
-            $(element).css("transition-delay",delay)
-            setTimeout(() => {
-                $(element).removeClass("opacity-0")
-                $(element).removeClass("translate-y-10")    
-                setTimeout(() => {
-                    $(element).removeClass("duration-500")      
-                    $(element).addClass("duration-150")      
-                }, delay)
-            },delay)
-            delay += 75
-        });
-
-        $("#follow-social-jombotron > a").each(function (index, element) {
-            $(element).css("transition-delay",delay)
-            setTimeout(() => {
-                $(element).removeClass("opacity-0")
-                $(element).removeClass("translate-y-10")    
-                setTimeout(() => {
-                    $(element).removeClass("duration-500")      
-                    $(element).removeClass("delay-[1200ms]")          
-                }, delay)
-            },delay)
-            delay += 75
-        });
-
-        setTimeout(() => {
-            $("#im-a-title,#intro-msg,#main-title-container,#button-download-cv,#follow-title").removeClass("opacity-0")            
-            $("#im-a-title,#intro-msg,#main-title-container,#button-download-cv,#follow-title").removeClass("-translate-x-10")      
-            $("#my-photo").removeClass("opacity-0")      
-            $("#my-photo").removeClass("translate-y-10")      
-        }, 100);
-        
-    },500)
-
-})
-
-$(document).ready(() => {
 
     const navHeight = $("nav").height()
     $(window).on("scroll", () => {
@@ -81,19 +29,15 @@ $(document).ready(() => {
     let isTextAppear = true
     const toggleTitle = () => {
         if(isTextAppear){
-            $("#main-title2").addClass("opacity-0")
-            $("#main-title2").addClass("translate-y-5")
+            $("#main-title2").addClass("opacity-0 translate-y-5")
             setTimeout(() => {
-                $("#main-title1").removeClass("opacity-0")
-                $("#main-title1").removeClass("translate-y-5")
+                $("#main-title1").removeClass("opacity-0 translate-y-5")
             },500)
         }
         else {
-            $("#main-title1").addClass("opacity-0")
-            $("#main-title1").addClass("translate-y-5")
+            $("#main-title1").addClass("opacity-0 translate-y-5")
             setTimeout(() => {
-                $("#main-title2").removeClass("opacity-0")
-                $("#main-title2").removeClass("translate-y-5")
+                $("#main-title2").removeClass("opacity-0 translate-y-5")
             },500)
         }
         isTextAppear = !isTextAppear
